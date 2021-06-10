0782237c9f7966d29fe95e0d1aedd0e3532766ec
xshen061 <xshen061@uottawa.ca>
2021-06-10 18:19:52 -0400

    Refactor for Q2.2

--- Assignment2/grades/lib/grades/calculator.ex
+++ Assignment2/grades/lib/grades/calculator.ex
@@ -26,6 +26,12 @@ defmodule Grades.Calculator do
 	  
 	  {avg_homework, avg_labs} 
 	end
+
+  # Refactor for Question 2.2
+  def failed_to_participate(avg_homework, avg_exams, num_labs) do 
+    avg_homework < 0.4 || avg_exams < 0.4 || num_labs < 3
+  end
+
 	
 	def letter_grade(%{homework: homework, labs: labs, midterm: midterm, final: final}) do
 	
@@ -38,7 +44,7 @@ defmodule Grades.Calculator do
       |> Enum.reject(fn mark -> mark < 0.25 end)
       |> Enum.count()
 
-    if avg_homework < 0.4 || avg_exams < 0.4 || num_labs < 3 do
+    if failed_to_participate(avg_homework, avg_exams, num_labs) do
       "EIN"
     else
       mark = 0.2 * avg_labs + 0.3 * avg_homework + 0.2 * midterm + 0.3 * final
@@ -71,7 +77,7 @@ defmodule Grades.Calculator do
       |> Enum.reject(fn mark -> mark < 0.25 end)
       |> Enum.count()
 
-    if avg_homework < 0.4 || avg_exams < 0.4 || num_labs < 3 do
+    if failed_to_participate(avg_homework, avg_exams, num_labs) do
       0
     else
       mark = 0.2 * avg_labs + 0.3 * avg_homework + 0.2 * midterm + 0.3 * final
