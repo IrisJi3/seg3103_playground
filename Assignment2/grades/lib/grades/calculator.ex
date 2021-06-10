aa38e07f503ac0d6e2d274519f414747e2731253
xshen061 <xshen061@uottawa.ca>
2021-06-10 18:13:17 -0400

    Refactor for Q2.1 - Extract a helper method avg

--- Assignment2/grades/lib/grades/calculator.ex
+++ Assignment2/grades/lib/grades/calculator.ex
@@ -1,24 +1,15 @@
 defmodule Grades.Calculator do
   def percentage_grade(%{homework: homework, labs: labs, midterm: midterm, final: final}) do
-    avg_homework =
-      if Enum.count(homework) == 0 do
-        0
-      else
-        Enum.sum(homework) / Enum.count(homework)
-      end
 
-    avg_labs =
-      if Enum.count(labs) == 0 do
-        0
-      else
-        Enum.sum(labs) / Enum.count(labs)
-      end
+    # Refactored for Q2.1
+    {avg_homework, avg_labs} = avg(homework, labs)
 
     mark = 0.2 * avg_labs + 0.3 * avg_homework + 0.2 * midterm + 0.3 * final
     round(mark * 100)
   end
 
-  def letter_grade(%{homework: homework, labs: labs, midterm: midterm, final: final}) do
+	# Refactored for Q2.1
+  def avg(homework, labs) do
     avg_homework =
       if Enum.count(homework) == 0 do
         0
@@ -32,7 +23,14 @@ defmodule Grades.Calculator do
       else
         Enum.sum(labs) / Enum.count(labs)
       end
-
+	  
+	  {avg_homework, avg_labs} 
+	end
+	
+	def letter_grade(%{homework: homework, labs: labs, midterm: midterm, final: final}) do
+	
+	{avg_homework, avg_labs} = avg(homework, labs)
+	
     avg_exams = (midterm + final) / 2
 
     num_labs =
@@ -62,19 +60,9 @@ defmodule Grades.Calculator do
   end
 
   def numeric_grade(%{homework: homework, labs: labs, midterm: midterm, final: final}) do
-    avg_homework =
-      if Enum.count(homework) == 0 do
-        0
-      else
-        Enum.sum(homework) / Enum.count(homework)
-      end
-
-    avg_labs =
-      if Enum.count(labs) == 0 do
-        0
-      else
-        Enum.sum(labs) / Enum.count(labs)
-      end
+    
+	# Refactored for Question 2.1
+    {avg_homework, avg_labs} = avg(homework, labs)
 
     avg_exams = (midterm + final) / 2
 
