fc8abf2ee271fa6acb053ba34a8b1afe891f6789
Iris Ji <45611041+IrisJi3@users.noreply.github.com>
2021-06-10 22:35:36 -0400

    Update calculator_test.exs

--- Assignment2/grades/test/grades/calculator_test.exs
+++ Assignment2/grades/test/grades/calculator_test.exs
@@ -35,73 +35,73 @@ defmodule Grades.CalculatorTest do
   	test "test_D" do
 		assert "D" ==
 				Calculator.letter_grade(%{
-					homework: [0.4],
-					labs: [1, 1, 1],
-					midterm: 0.4,
-					final: 0.4
+					homework: [0.5],
+					labs: [0.5, 0.5, 0.5],
+					midterm: 0.5,
+					final: 0.5
 				})
 	end
   	test "test_D+" do
 		assert "D+" ==
 				Calculator.letter_grade(%{
-					homework: [0.6],
-					labs: [1, 1, 1],
-					midterm: 0.4,
-					final: 0.4
+					homework: [0.55],
+					labs: [0.55, 0.55, 0.55],
+					midterm: 0.55,
+					final: 0.55
 				})
 	end
   	test "test_C" do
 		assert "C" ==
 				Calculator.letter_grade(%{
-					homework: [0.7],
-					labs: [1, 1, 1],
-					midterm: 0.4,
-					final: 0.4
+					homework: [0.6],
+					labs: [0.6, 0.6, 0.6],
+					midterm: 0.6,
+					final: 0.6
 				})
 	end
   	test "test_C+" do
 		assert "C+" ==
 				Calculator.letter_grade(%{
-					homework: [1],
-					labs: [1, 1, 1],
-					midterm: 0.9,
-					final: 0
+					homework: [0.65],
+					labs: [0.65, 0.65, 0.65],
+					midterm: 0.65,
+					final: 0.65
 				})
 	end
   	test "test_B" do
 		assert "B" ==
 				Calculator.letter_grade(%{
-					homework: [1],
-					labs: [1, 1, 1],
-					midterm: 1,
-					final: 0.1
+					homework: [0.7],
+					labs: [0.7, 0.7, 0.7],
+					midterm: 0.7,
+					final: 0.7
 				})
 	end
   	test "test_B+" do
 		assert "B+" ==
 				Calculator.letter_grade(%{
-					homework: [1],
-					labs: [1, 1, 1],
-					midterm: 1,
-					final: 0.2
+					homework: [0.75],
+					labs: [0.75, 0.75, 0.75],
+					midterm: 0.75,
+					final: 0.75
 				})
 	end
   	test "test_A-" do
 		assert "A-" ==
 				Calculator.letter_grade(%{
-					homework: [1],
-					labs: [1, 1, 1],
-					midterm: 1,
-					final: 0.35
+					homework: [0.8],
+					labs: [0.8, 0.8, 0.8],
+					midterm: 0.8,
+					final: 0.8
 				})
 	end
   	test "test_A" do
 		assert "A" ==
 				Calculator.letter_grade(%{
-					homework: [1],
-					labs: [1, 1, 1],
-					midterm: 1,
-					final: 0.5
+					homework: [0.85],
+					labs: [0.85, 0.9, 0.85],
+					midterm: 0.85,
+					final: 0.85
 				})
 	end
   	test "test_A+" do
@@ -137,73 +137,74 @@ end
   	test "test_2" do
 		assert 2 ==
 				Calculator.numeric_grade(%{
-					homework: [0.4],
-					labs: [1, 1, 1],
-					midterm: 0.4,
-					final: 0.4
+					homework: [0.5],
+					labs: [0.5, 0.5, 0.5],
+					midterm: 0.5,
+					final: 0.5
 				})
 	end
   	test "test_3" do
 		assert 3 ==
 				Calculator.numeric_grade(%{
-					homework: [0.6],
-					labs: [1, 1, 1],
-					midterm: 0.4,
-					final: 0.4
+					homework: [0.55],
+					labs: [0.55, 0.55, 0.55],
+					midterm: 0.55,
+					final: 0.55
 				})
 	end
   	test "test_4" do
 		assert 4 ==
 				Calculator.numeric_grade(%{
-					homework: [0.7],
-					labs: [1, 1, 1],
-					midterm: 0.4,
-					final: 0.4
+					homework: [0.6],
+					labs: [0.6, 0.6, 0.6],
+					midterm: 0.6,
+					final: 0.6
 				})
 	end
   	test "test_5" do
 		assert 5 ==
 				Calculator.numeric_grade(%{
-					homework: [1],
-					labs: [1, 1, 1],
-					midterm: 0.9,
-					final: 0
+					homework: [0.65],
+					labs: [0.65, 0.65, 0.65],
+					midterm: 0.65,
+					final: 0.65
 				})
 	end
   	test "test_6" do
 		assert 6 ==
 				Calculator.numeric_grade(%{
-					homework: [1],
-					labs: [1, 1, 1],
-					midterm: 1,
-					final: 0.1
+					homework: [0.7],
+					labs: [0.7, 0.7, 0.7],
+					midterm: 0.7,
+					final: 0.7
 				})
 	end
   	test "test_7" do
 		assert 7 ==
 				Calculator.numeric_grade(%{
-					homework: [1],
-					labs: [1, 1, 1],
-					midterm: 1,
-					final: 0.2
+					homework: [0.75],
+					labs: [0.75, 0.75, 0.75],
+					midterm: 0.75,
+					final: 0.75
+				})
 				})
 	end
   	test "test_8" do
 		assert 8 ==
 				Calculator.numeric_grade(%{
-					homework: [1],
-					labs: [1, 1, 1],
-					midterm: 1,
-					final: 0.35
+					homework: [0.8],
+					labs: [0.8, 0.8, 0.8],
+					midterm: 0.8,
+					final: 0.8
 				})
 	end
   	test "test_9" do
 		assert 9 ==
 				Calculator.numeric_grade(%{
-					homework: [1],
-					labs: [1, 1, 1],
-					midterm: 1,
-					final: 0.5
+					homework: [0.85],
+					labs: [0.85, 0.9, 0.85],
+					midterm: 0.85,
+					final: 0.85
 				})
 	end
   	test "test_10" do
